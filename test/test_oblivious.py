a2077d292e56e2efcdedf8a968a8f62681de2ed5
Andrei Lapets <a@lapets.io>
2020-08-04 02:02:23 -0400

    Refactor unit tests; test both native Python and libsodium wrapper methods.

--- test/test_oblivious.py
+++ test/test_oblivious.py
@@ -21,37 +21,85 @@ def check_or_generate_operation(self, fun, lengths, bits):
     )
     return check_or_generate(self, fs, bits)
 
-class Test_oblivious(TestCase):
-    def test_scalar(self, bits='4df8fe738c097afa7f255b10c3ab118eeb73e38935605042ccb7581c73f1e5e9'):
-        def fun(bs):
-            return bitlist([1 if scalar(bs) else 0])
-        return check_or_generate_operation(self, fun, [32], bits)
+def check_scalar(self, cls, bits=None):
+    bits = bits if bits is not None else\
+        '4df8fe738c097afa7f255b10c3ab118eeb73e38935605042ccb7581c73f1e5e9'
+    fun = lambda bs: return bitlist([1 if cls.scalar(bs) else 0])
+    return check_or_generate_operation(self, fun, [32], bits)
 
-    def test_base(self, bits='080874618c0878927620101043a31002e840818101204000401210101261c120'):
-        def fun(bs):
-            return base(bs) if scalar(bs) else bytes([0])
-        return check_or_generate_operation(self, fun, [32], bits)
+def check_base(self, cls, bits=None):
+    bits = bits if bits is not None else\
+        '080874618c0878927620101043a31002e840818101204000401210101261c120'
+    fun = lambda bs: cls.base(bs) if cls.scalar(bs) else bytes([0])
+    return check_or_generate_operation(self, fun, [32], bits)
 
-    def test_mul(self, bits='28c5004000000100850000002102088891100000000081080810004280080004'):
-        def fun(bs):
-            (bs1, bs2) = parts(bs, length=32)
-            return mul(bs1, base(bs2)) if scalar(bs1) and scalar(bs2) else bytes([0])
-        return check_or_generate_operation(self, fun, [32, 32], bits)
+def check_mul(self, cls, bits=None):
+    bits = bits if bits is not None else\
+        '28c5004000000100850000002102088891100000000081080810004280080004'
+    def fun(bs):
+        (bs1, bs2) = parts(bs, length=32)
+        return\
+            cls.mul(bs1, cls.base(bs2))\
+            if cls.scalar(bs1) and cls.scalar(bs2) else\
+            bytes([0])
+    return check_or_generate_operation(self, fun, [32, 32], bits)
 
-    def test_add(self, bits='0844000040000002818040008400000031080028000081080801000204081800'):
-        def fun(bs):
-            (bs1, bs2) = parts(bs, length=32)
-            return add(base(bs1), base(bs2)) if scalar(bs1) and scalar(bs2) else bytes([0])
-        return check_or_generate_operation(self, fun, [32, 32], bits)
+def check_add(self, cls, bits=None):
+    bits = bits if bits is not None else\
+        '0844000040000002818040008400000031080028000081080801000204081800'
+    def fun(bs):
+        (bs1, bs2) = parts(bs, length=32)
+        return\
+            cls.add(cls.base(bs1), cls.base(bs2))\
+            if scalar(bs1) and cls.scalar(bs2) else\
+            bytes([0])
+    return check_or_generate_operation(self, fun, [32, 32], bits)
 
-    def test_sub(self, bits='002400041000800280800000a5024408111000800000810000008040a0081040'):
-        def fun(bs):
-            (bs1, bs2) = parts(bs, length=32)
-            return sub(base(bs1), base(bs2)) if scalar(bs1) and scalar(bs2) else bytes([0])
-        return check_or_generate_operation(self, fun, [32, 32], bits)
+def check_sub(self, cls, bits=None):
+    bits = bits if bits is not None else\
+        '002400041000800280800000a5024408111000800000810000008040a0081040'
+    def fun(bs):
+        (bs1, bs2) = parts(bs, length=32)
+        return\
+            cls.sub(cls.base(bs1), cls.base(bs2))\
+            if cls.scalar(bs1) and cls.scalar(bs2) else\
+            bytes([0])
+    return check_or_generate_operation(self, fun, [32, 32], bits)
+
+class Test_native(TestCase):
+    def test_scalar(self, bits=None):
+        return check_scalar(self, native, bits)
+
+    def test_base(self, bits=None):
+        return check_base(self, native, bits)
+
+    def test_mul(self, bits=None):
+        return check_mul(self, native, bits)
+
+    def test_add(self, bits=None):
+        return check_add(self, native, bits)
+
+    def test_sub(self, bits=None):
+        return check_sub(self, native, bits)
+
+class Test_sodium(TestCase):
+    def test_scalar(self, bits=None):
+        return check_scalar(self, sodium, bits)
+
+    def test_base(self, bits=None):
+        return check_base(self, sodium, bits)
+
+    def test_mul(self, bits=None):
+        return check_mul(self, sodium, bits)
+
+    def test_add(self, bits=None):
+        return check_add(self, sodium, bits)
+
+    def test_sub(self, bits=None):
+        return check_sub(self, sodium, bits)
 
 if __name__ == "__main__":
     # Generate reference bit lists for tests.
-    test_oblivious = Test_oblivious()
+    test_oblivious = Test_native()
     for m in [m for m in dir(test_oblivious) if m.startswith('test_')]:
         print(m + ': ' + getattr(test_oblivious, m)(bits=None))
