f119998e873237bf2cbddd57724aef59410dc2f0
Andrei Lapets <a@lapets.io>
2020-08-26 02:07:30 -0400

    Update dependency and package versions.

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="oblivious",
-    version="0.1.1",
+    version="0.2.0",
     packages=["oblivious",],
     install_requires=[
-        "fe25519>=0.1.2",
-        "ge25519>=0.1.2",
+        "fe25519>=0.2.0",
+        "ge25519>=0.2.0",
         "parts>=0.2.1",
         "bitlist>=0.3.1",
         "fountains>=0.2.1",
