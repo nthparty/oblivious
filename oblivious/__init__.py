131b2e8573207d4b62ac191fc9848d40b1150f6a
Andrei Lapets <a@lapets.io>
2020-09-01 23:52:07 -0400

    Add new hash-to-point and scalar inversion methods to namespace.

--- oblivious/__init__.py
+++ oblivious/__init__.py
@@ -1,4 +1,6 @@
 """Allow users to use values directly."""
 from oblivious.oblivious import\
     native, sodium,\
-    scalar, rand, base, mul, add, sub
+    scalar, rand, inv,\
+    pnt, base,\
+    mul, add, sub
