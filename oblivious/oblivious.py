b2016ff4cfcb34e67ca00d507e91f5453d2b944b
Andrei Lapets <a@lapets.io>
2020-11-15 03:01:04 -0500

    Add linting rule exceptions for backwards compatibility.

--- oblivious/oblivious.py
+++ oblivious/oblivious.py
@@ -280,13 +280,13 @@ class scalar(bytes):
         """
         return inv(self)
 
-    def __mul__(self: scalar, other: Union[scalar, point]) -> Union[scalar, point]:
+    def __mul__(self: scalar, other: Union[scalar, point]) -> Union[scalar, point]: # pylint: disable=E1136
         """Multiply supplied scalar or point by this scalar."""
         if isinstance(other, (native.scalar, sodium.scalar)):
             return smu(self, other)
         return mul(self, other)
 
-    def __rmul__(self: scalar, other: Union[scalar, point]) -> Union[scalar, point]:
+    def __rmul__(self: scalar, other: Union[scalar, point]) -> Union[scalar, point]: # pylint: disable=E1136
         """A scalar cannot be on the right-hand side of a non-scalar."""
         raise TypeError('scalar must be on left-hand side of multiplication operator')
 
@@ -468,13 +468,13 @@ try:
             """
             return inv(self)
 
-        def __mul__(self: scalar, other: Union[scalar, point]) -> Union[scalar, point]:
+        def __mul__(self: scalar, other: Union[scalar, point]) -> Union[scalar, point]: # pylint: disable=E1136
             """Multiply supplied scalar or point by this scalar."""
             if isinstance(other, (native.scalar, sodium.scalar)):
                 return smu(self, other)
             return mul(self, other)
 
-        def __rmul__(self: scalar, other: Union[scalar, point]) -> Union[scalar, point]:
+        def __rmul__(self: scalar, other: Union[scalar, point]) -> Union[scalar, point]: # pylint: disable=E1136
             """A scalar cannot be on the right-hand side of a non-scalar."""
             raise TypeError('scalar must be on left-hand side of multiplication operator')
 
