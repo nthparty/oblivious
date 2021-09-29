1f39a4c40c852b4ad2dc101c1e5a6095023e86b0
Andrei Lapets <a@lapets.io>
2021-09-29 01:51:44 -0400

    Filter Sphinx refs from README text in package build steps.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 with open("README.rst", "r") as fh:
-    long_description = fh.read()
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
 
 # The lines below are parsed by `docs/conf.py`.
 name = "oblivious"
